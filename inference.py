14cd75530d92bf646838c6466cc2c4e0755d4552
Arthur Galdino Dangoni <oniyoumu@gmail.com>
2025-05-19 17:45:54 -0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,16 +2,21 @@
 from model import PointCloudNet
 from utils import predict
 import torch
+import torch.nn as nn
 
 
 
-model_save_name = "pc1024_three.pth"
+model_save_name = "/content/RGB2point/pc1024_three.pth"
 
 model = PointCloudNet(num_views=1, point_cloud_size=1024, num_heads=4, dim_feedforward=2048)
-model.load_state_dict(torch.load(model_save_name)["model"])
+state = torch.load(model_save_name)
+if "model" in state:
+    model.load_state_dict(state["model"])
+else:
+    model.load_state_dict(state)
 model.eval()  
 
-image_path = "img/1013.jpg"
-save_path = "result/1013.ply"
+image_path = "/content/RGB2point/img/fish.png"
+save_path = "result/fish.ply"
 
 predict(model, image_path, save_path)
